324f57a733a34d918be6bf1b6cafd9c3fccbec64
Ricardo Branco <rbranco@suse.de>
2023-09-17 20:44:10 +0200

    Update Terminator plugin

--- terminator.py
+++ terminator.py
@@ -10,19 +10,21 @@ https://terminator-gtk3.readthedocs.io/en/latest/plugins.html
 import re
 import terminatorlib.plugin as plugin
 
-AVAILABLE = ['cve', 'suse_bugzilla', 'suse_jira', 'suse_incident', 'suse_fate', 'suse_progress']
+AVAILABLE = ['CVE', 'SUSE_Bugzilla', 'SUSE_Jira', 'SUSE_Incident', 'SUSE_Progress']
 
 
-class base(plugin.URLHandler):
+class Base(plugin.URLHandler):
     capabilities = ['url_handler']
+    _extract = ""
+    _url = ""
 
-    def callback(self, line):
-        for item in re.findall(self._extract, line):
-            return self._url + item
+    def callback(self, url):
+        for item in re.findall(self._extract, url):
+            return f"{self._url}{item}"
 
 
-class cve(base):
-    handler_name = 'cve'
+class CVE(Base):
+    handler_name = 'CVE'
     match = r'\bCVE-[0-9]+-[0-9]+\b'
     nameopen = "Open CVE item"
     namecopy = "Copy CVE URL"
@@ -30,17 +32,17 @@ class cve(base):
     _url = 'https://cve.mitre.org/cgi-bin/cvename.cgi?name='
 
 
-class suse_bugzilla(base):
-    handler_name = 'suse_bugzilla'
-    match = r'\b(bsc|bnc)#[0-9]+\b'
+class SUSE_Bugzilla(Base):
+    handler_name = 'SUSE_Bugzilla'
+    match = r'\b(bsc|bnc|boo)#[0-9]+\b'
     nameopen = "Open Bugzilla item"
     namecopy = "Copy Bugzilla URL"
     _extract = '[0-9]+'
     _url = 'https://bugzilla.suse.com/show_bug.cgi?id='
 
 
-class suse_jira(base):
-    handler_name = 'suse_jira'
+class SUSE_Jira(Base):
+    handler_name = 'SUSE_Jira'
     match = r'\bjsc#[A-Z]+-[0-9]+\b'
     nameopen = "Open Jira item"
     namecopy = "Copy Jira URL"
@@ -48,8 +50,8 @@ class suse_jira(base):
     _url = 'https://jira.suse.com/browse/'
 
 
-class suse_incident(base):
-    handler_name = 'suse_incident'
+class SUSE_Incident(Base):
+    handler_name = 'SUSE_Incident'
     match = r'\bS(USE)?:M(aintenance)?:[0-9]+:[0-9]+\b'
     nameopen = "Open Incident item"
     namecopy = "Copy Incident URL"
@@ -57,17 +59,8 @@ class suse_incident(base):
     _url = 'https://smelt.suse.de/incident/'
 
 
-class suse_fate(base):
-    handler_name = 'suse_fate'
-    match = r'\bFATE:[0-9]+\b'
-    nameopen = "Open FATE item"
-    namecopy = "Copy FATE URL"
-    _extract = '[0-9]+'
-    _url = 'https://fate.suse.com/'
-
-
-class suse_progress(base):
-    handler_name = 'suse_progress'
+class SUSE_Progress(Base):
+    handler_name = 'SUSE_Progress'
     match = r'\bpoo#[0-9]+\b'
     nameopen = "Open POO item"
     namecopy = "Copy POO URL"
